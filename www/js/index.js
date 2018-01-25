7f5dcbae3f11729085208649e1e6783da88b9195
alexandrugavril <alex@wavemotionlabs.com>
2018-01-25 15:16:16 +0200

    Finished user page

--- www/js/index.js
+++ www/js/index.js
@@ -28,6 +28,83 @@ function checkPreAuth() {
     }
 }
 
+function changeIcon(domImg,srcImage)
+{
+    var img = new Image();
+    img.onload = function()
+    {
+        // Load completed
+        domImg.src = this.src;
+    };
+    img.src = srcImage;
+}
+
+function updateLogo(type)
+{
+    var logo = document.getElementById("logo");
+    console.log(logo);
+    switch(type){
+        case ('medication'):
+            changeIcon(logo, 'img/pill-2-xxl.png');
+            console.log("medication");
+            break;
+        case ('appointment'):
+            changeIcon(logo, 'img/journal-menu.png');
+            console.log("it's an appointment appointment");
+            break;
+        case ('weight'):
+            changeIcon(logo, 'img/weight-warning.png');
+            console.log("weight");
+            break;
+        case ('heart'):
+            changeIcon(logo, 'img/heart-ok.png');
+            console.log("heart");
+            break;
+        case ('steps'):
+            changeIcon(logo, 'img/steps-ok.png');
+            console.log("steps");
+            break;
+        default:
+            return
+
+    }
+}
+
+function previousReminder()
+{
+    app.setCurrentReminder(app.currentReminder - 1);
+}
+
+function nextReminder()
+{
+    app.setCurrentReminder(app.currentReminder + 1);
+}
+
+function getReminders()
+{
+    $.ajax({
+        url: "http://cami.vitaminsoftware.com:8008/api/v1/journal_entries/?user=2",
+        dataType: "json",
+        type: 'GET',
+        success: function (data) {
+            var rems = data.objects;
+            console.log(rems);
+            app.reminders = rems;
+            var cnt = 0;
+            var nCnt = 0;
+            for (var i = 0; i < rems.length; i++) {
+                if (rems[i].acknowledged === true || rems[i].acknowledged === false) {
+                    nCnt = nCnt + 1;
+                }
+                else {
+                    app.setCurrentReminder(i);
+                    break;
+                }
+            }
+        }
+    });
+}
+
 function handleLogin() {
     console.log("handling");
     var form = $("#loginForm");
@@ -42,7 +119,7 @@ function handleLogin() {
         if(u === 'catalin' && p === 'cami') {
             window.localStorage["username"] = u;
             window.localStorage["password"] = p;
-            $.mobile.changePage("UserPage.html");
+            $.mobile.navigate("#home-page", { transition : "slide"});
         } else {
             navigator.notification.alert("Your login failed", function() {});
             $("#submitButton").removeAttr("disabled");
@@ -61,6 +138,25 @@ function handleLogin() {
 var app = {
     SOME_CONSTANTS : false,  // some constant
 
+    reminders: [],
+    currentReminder: -1,
+    setCurrentReminder: function(i)
+    {
+        if(i >= this.reminders.length)
+        {
+            this.currentReminder = 0;
+        }
+        else if(i < 0)
+        {
+            this.currentReminder = this.reminders.length - 1;
+        }
+        else{
+            this.currentReminder = i;
+        }
+
+        $('#reminderContent').html(this.reminders[this.currentReminder].message);
+        updateLogo(this.reminders[this.currentReminder].type);
+    },
 
     // Application Constructor
     initialize: function() {
